9eee260dcc40df0ad0e30a5f44b3e01c8cd5d171
paveldm-study <159199264+paveldm-study@users.noreply.github.com>
2024-02-07 17:50:57 +0300

    Update Main.java

--- src/main/java/org/dpa/myarraylist/Main.java
+++ src/main/java/org/dpa/myarraylist/Main.java
@@ -2,6 +2,6 @@ package org.dpa.myArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayList<Integer> arrayList = new MyArrayList<>();
     }
 }
